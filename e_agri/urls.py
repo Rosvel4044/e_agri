3571d254f42520b71078ee62f2f3f0a195b4c587
Rosvel4044 <150371162+Rosvel4044@users.noreply.github.com>
2026-01-27 03:52:02 -0800

    Update urls.py

--- e_agri/urls.py
+++ e_agri/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 #from .views import contact,index,profile
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     #path('',index,name="home"),
