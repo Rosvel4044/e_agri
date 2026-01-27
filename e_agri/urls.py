beb05230271a8e1d7188e801a7680ae525fd5573
Rosvel4044 <150371162+Rosvel4044@users.noreply.github.com>
2026-01-27 04:00:47 -0800

    Comment out accueil path in e_agri/urls.py
    Comment out the accueil path in urls.py

--- e_agri/urls.py
+++ e_agri/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     #path('contact',contact,name="contact"),
     #path('profile',profile,name="profil"),
     path('agri_market/', include('agri_market.urls')),
-    path('', views.accueil, name='accueil'),
+    #path('', views.accueil, name='accueil'),
     path('admin/', admin.site.urls),
 ]
