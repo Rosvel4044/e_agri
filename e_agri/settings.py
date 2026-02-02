b57218ab97e38118e01a2d35155f88492c4c122c
Rosvel4044 <150371162+Rosvel4044@users.noreply.github.com>
2026-02-02 05:21:34 -0800

    Update settings.py

--- e_agri/settings.py
+++ e_agri/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'agri_market'
+    'agri_market',
+    'produits'
 ]
 
 MIDDLEWARE = [
