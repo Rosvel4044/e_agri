9367f53267a7e5e37470e0ca5d24b5e30a9db262
Rosvel4044 <150371162+Rosvel4044@users.noreply.github.com>
2026-02-04 10:56:45 -0800

    Update urls.py

--- agri_market/urls.py
+++ agri_market/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     
     
     # URLs publiques
-    path('liste', views.liste_produits, name='liste_produits'),
+    path('liste', views.liste_produits, name='liste_produit'),
     path('produit/<int:produit_id>/', views.detail_produit, name='detail_produit'),
     
         # URLs client - Panier
